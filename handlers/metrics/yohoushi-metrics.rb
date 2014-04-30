f2f3ac6fccc1f8b5e575af3381df7c6acf27015a
mdoi <mdoi@kaizenplatform.in>
2014-04-30 18:02:07 +0900

    add yohoushi metric

---
+++ handlers/metrics/yohoushi-metrics.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+#
+# Copyright 2013 FunGo Studios (team@fungostudios.com)
+#
+# Depends on librato-metrics gem
+# gem install librato-metrics
+#
+# Released under the same terms as Sensu (the MIT license); see LICENSE
+# for details.
+
+require 'rubygems' if RUBY_VERSION < '1.9.0'
+require 'sensu-handler'
+require "net/http"
+require "uri"
+
+class YohoushiMetrics < Sensu::Handler
+  # override filters from Sensu::Handler. not appropriate for metric handlers
+  def filter
+  end
+
+  def handle
+    @event['check']['output'].split("\n").each do |line|
+      matches = REGEXP.match(line)
+      begin
+        timeout(3) do
+          if matches
+            Net::HTTP.start(settings['yohoushi']['host'], settings['yohoushi']['port']){|http|
+              body = "number=#{matches[:value]}"
+              response = http.post(matches[:key], body)
+            }
+          end
+        end
+      rescue Timeout::Error
+        puts "yohoushi -- timed out while sending metrics"
+      rescue => error
+        puts "yohoushi -- failed to send metrics : #{error.message}"
+        puts "  #{error.backtrace.join("\n\t")}"
+      end
+    end
+  end
+end
